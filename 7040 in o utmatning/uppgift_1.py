f3837b79e91cdcef7c8501f2253d724c2fabd834
Ella Aronsson <Ella.aronsson06@skola.stenungsund.se>
2024-09-02 11:04:29 +0200

    klar

---
+++ 7040 in o utmatning/uppgift_1.py
@@ -0,0 +1,8 @@
+namn = input("vad heter du?")
+print ("hej", namn, "!")
+print ("vad är din favoritfärg?")
+input("rikard din favoritfärg är lila.")
+       
+       
+       
+    
--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
 Ella Aronsson loggbok
 =====================
+24-09-02
+---------
+gjort:
+
+*exempel 1 
+*7040 uppgift 1 
+
 24-08-26
 -----------
 jag har installerat VS Codium
