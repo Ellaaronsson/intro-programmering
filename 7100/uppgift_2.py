b20cf01b1941a74550c41c63365fb8753e81b22e
Ella Aronsson <Ella.aronsson06@skola.stenungsund.se>
2024-11-18 10:51:39 +0100

    klar

--- 7100/uppgift_2.py
+++ 7100/uppgift_2.py
@@ -2,13 +2,10 @@ import random
 tal_lista = list(range(2, 100))
 primtal = []
 while tal_lista:
-    p=tal_lista.pop(0)
-    primtal.append(p)
-    tal_lista = tal_lista - 1 
-    
-
-
-elif tal_lista:
-    print("skriv ut primtal:")
+    prim=tal_lista.pop(0)
+    primtal.append(prim)
+    tal_lista = [tal for tal in tal_lista if tal % prim != 0]
+primtal.reverse()
+print(primtal)
 
 
