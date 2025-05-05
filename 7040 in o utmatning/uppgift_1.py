452420bcc4fff0808a5e25d1a710d4fdc067df79
Ella Aronsson <Ella.aronsson06@skola.stenungsund.se>
2025-05-05 10:20:46 +0200

    klar

--- 7040 in o utmatning/uppgift_1.py
+++ 7040 in o utmatning/uppgift_1.py
@@ -2,8 +2,3 @@ namn = input("vad heter du?")
 print ("hej", namn, "!")
 färg= print ("vad är din favoritfärg?")
 input(namn, "din favoritfärg är", färg)
-
-       
-       
-       
-    
---
+++ 7060 slingor - while/Uppgift_3.py
@@ -0,0 +1,5 @@
+index = 1
+while index <= 30:
+    print(index)
+    index= index + 1
+    
